2462782eca6b987617ca4ded6f0a257f74f8fe2b
User <Kozheletovan@mail.ru>
2022-10-15 21:22:13 +0500

    decision

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var width: Int = 7
+    var depth: Int = 3
+    var height: Int = 5
+    var volume: Int = width * depth * height
+    println("result: volum $volume")
+}
+
+
+
